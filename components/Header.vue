d81ec950afba372f209786c5087a039f7549949a
mahima98 <mahimaramgolam98@gmail.com>
2024-07-11 21:53:26 +0400

    Added header

---
+++ components/Header.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { NavItem } from '@nuxt/content'
+
+
+</script>
+
+<template>
+  <UHeader >
+    <template #logo>
+       CalorieTracker
+    </template>
+
+    <template #center>
+        <UButton color="primary" variant="solid" label="Button">
+            <i class="i-heroicons-plus"></i>
+            <span>Create new list</span>
+        </UButton>
+    </template>
+
+    <template #right>
+      <UColorModeButton />
+      <UButton to="https://github.com/nuxt/ui" target="_blank" icon="i-simple-icons-github" color="gray" variant="ghost" />
+    </template>
+
+  </UHeader>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  extends: ['@nuxt/ui-pro'],
+  modules: ['@nuxt/ui',  '@nuxtjs/tailwindcss'],
   devtools: { enabled: true }
 })
